58374cc57de4731bd9d04818c3cbb42ed09f9d3f
Roman Rzyanin <roman@rzyanin.ru>
2024-02-15 21:53:52 +0300

    lesson5:add homework

---
+++ homework/Program.cs
@@ -0,0 +1,24 @@
+﻿// task1
+/* Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+ в промежутке от M до N. Использовать рекурсию, не использовать циклы.*/
+
+
+
+void ShowNumbers (int start, int end)
+{
+    // Базовый случай
+    if (start == end)
+    {
+        Console.Write(start);
+        return;
+    }
+    // Рекурсивный случай
+    Console.Write(start + " ");
+    ShowNumbers(start + 1, end);
+}
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+ShowNumbers(M, N); // start = M, end = N
---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// // task1
+
+double GetFact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        //Console.WriteLine($"Stop:{n}");
+        return 1;
+    }
+    //Console.WriteLine(n);
+    return n * GetFact(n - 1);
+    
+}
+Console.Write(GetFact(123));
+
+// void OpenMatryoshka(int size)
+// {
+//     Console.WriteLine($"Opening matryoshka of size {size}...");
+//     if (size == 1)
+//     {
+//         Console.WriteLine($"Reached the smallest matryoshka!");
+//         return;
+//     }
+//     OpenMatryoshka(size - 1);
+//     //Console.WriteLine($"Opening matryoshka of size {size}...");
+// }
+// OpenMatryoshka(5);
