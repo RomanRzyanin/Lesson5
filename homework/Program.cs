a6d987e775119c27b3d79d17830683265f1655a6
Roman Rzyanin <roman@rzyanin.ru>
2024-02-15 22:07:19 +0300

    lesson5:add homework1

--- homework/Program.cs
+++ homework/Program.cs
@@ -2,23 +2,34 @@
 /* Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
  в промежутке от M до N. Использовать рекурсию, не использовать циклы.*/
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="start"></param>
+/// <param name="end"></param>
+// void ShowNumbers (int start, int end)
+// {
+//     // Базовый случай
+//     if (start == end)
+//     {
+//         Console.Write(start);
+//         return;
+//     }
+//     // Рекурсивный случай
+//     Console.Write(start + " ");
+//     ShowNumbers(start + 1, end);
+// }
+// Console.Write("Введите число M: ");
+// int M = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+// ShowNumbers(M, N); // start = M, end = N
+
+//task2
+/*Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.*/
 
 
-void ShowNumbers (int start, int end)
-{
-    // Базовый случай
-    if (start == end)
-    {
-        Console.Write(start);
-        return;
-    }
-    // Рекурсивный случай
-    Console.Write(start + " ");
-    ShowNumbers(start + 1, end);
-}
-Console.Write("Введите число M: ");
-int M = int.Parse(Console.ReadLine()!);
-Console.Write("Введите число N: ");
-int N = int.Parse(Console.ReadLine()!);
-
-ShowNumbers(M, N); // start = M, end = N
+//task3
+/*Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.*/
