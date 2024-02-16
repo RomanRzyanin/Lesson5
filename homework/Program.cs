ff4cdc2a3e1f5cb3b0f66c11e9e4b58cfe513c63
Roman Rzyanin <roman@rzyanin.ru>
2024-02-16 18:49:59 +0300

    lesson5:homework_is_solved

--- homework/Program.cs
+++ homework/Program.cs
@@ -3,10 +3,10 @@
  в промежутке от M до N. Использовать рекурсию, не использовать циклы.*/
 
 /// <summary>
-/// 
+/// Метод печатает все целые числа от M до N
 /// </summary>
-/// <param name="start"></param>
-/// <param name="end"></param>
+/// <param name="start"> точка начала, M </param>
+/// <param name="end"> точка остановки, N </param>
 // void ShowNumbers (int start, int end)
 // {
 //     // Базовый случай
@@ -23,13 +23,63 @@
 // int M = int.Parse(Console.ReadLine()!);
 // Console.Write("Введите число N: ");
 // int N = int.Parse(Console.ReadLine()!);
-// ShowNumbers(M, N); // start = M, end = N
+// ShowNumbers(M, N);
 
 //task2
 /*Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 Даны два неотрицательных числа m и n.*/
 
+/// <summary>
+/// Метод вычисляющий функцию Аккермана
+/// </summary>
+/// <param name="n"> первый параметр функции Аккермана </param>
+/// <param name="m"> второй параметр функции Аккермана </param>
+/// <returns> Результат вычисления Функции Аккермана</returns>
+// double GetAckermanFunction(int n, double m)
+// {
+//     //Базовый случай
+//     if (n == 0)
+//     {
+//         return m + 1;
+//     }
+//     // Рекурсивный случай
+//     else if (m == 0)
+//     {
+//         return GetAckermanFunction(n - 1, 1);
+//     }
+//     else
+//     {
+//         return GetAckermanFunction(n - 1, GetAckermanFunction(n, m - 1));
+//     }
+// }
+// Console.Write("Введите число n: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число m: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Функция Аккермана ({n}, {m}) = {GetAckermanFunction(n, m)}");
 
 //task3
 /*Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
 Использовать рекурсию, не использовать циклы.*/
+
+/// <summary>
+/// Метод печатает элементы архива в обратном порядке.
+/// </summary>
+/// <param name="array"> архив </param>
+/// <param name="j"> индекс последнего элемента архива </param>
+void PrintReverseArray (int[] array, int j)
+{
+    //Базовый случай
+    if (j == 0)
+    {
+        Console.Write($"{array[j]} ]");
+        return;
+    }
+    //Рекурсивный случай
+    Console.Write($"{array[j]}, ");
+    PrintReverseArray(array, j - 1);
+}
+int[] array = { 2, 3, 6, 33, 57, 0, 45, 667, 89, 5, 11 };
+int l = array.Length - 1;
+Console.Write("[ ");
+PrintReverseArray(array, l);
